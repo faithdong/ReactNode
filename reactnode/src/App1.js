3d7068fd7cea5e538717d19eb1f7e953a06b3f0a
zhongxiaodong <zxdzyadmin@yonyou.com>
2019-02-25 17:57:56 +0800

    mdy

--- reactnode/src/App1.js
+++ reactnode/src/App1.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import { addGun , removeGun , addGunAsync } from './Index.redux';
 
+// const mapStatetoProps = (state)=>{
+//   return {num:state}
+// }
+// const actionCreators = {addGun , removeGun , addGunAsync};
+//App1 = connect(mapStatetoProps,actionCreators)(App1);
+@connect(
+  state=>({num:state}),
+  {addGun , removeGun , addGunAsync}
+)
 class App1 extends React.Component{
 
   render(){
-    const store = this.props.store;
-    const num = store.getState();
-    const addGun = this.props.addGun;
-    const removeGun = this.props.removeGun;
-    const addGunAsync = this.props.addGunAsync;
     return(
       <div>
-        <h1>现在有机关枪{num}把</h1>
+        <h1>现在有机关枪{this.props.num}把</h1>
 
-        <button onClick={()=>store.dispatch(addGun())}>申请武器</button>
-        <button onClick={()=>store.dispatch(removeGun())}>归还武器</button>
-        <button onClick={()=>store.dispatch(addGunAsync())}>拖两天</button>
+        <button onClick={this.props.addGun}>申请武器</button>
+        <button onClick={this.props.removeGun}>归还武器</button>
+        <button onClick={this.props.addGunAsync}>拖两天</button>
       </div>
     )
   }
--- reactnode/src/index.js
+++ reactnode/src/index.js
@@ -20,30 +20,60 @@ import App1 from './App1';
 import { createStore , applyMiddleware , compose }  from 'redux';
 import thunk  from 'redux-thunk';
 import { Provider } from 'react-redux';
-
+import { BrowserRouter , Route , Link , Redirect , Switch } from 'react-router-dom';
 import {counter} from './Index.redux';
-import { addGun , removeGun , addGunAsync } from './Index.redux';
+//import { addGun , removeGun , addGunAsync } from './Index.redux'; 
 
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 // const store = createStore(counter, /* preloadedState, */ composeEnhancers(
 //   applyMiddleware(thunk),
 // ));
-const reduxDevTools = window.devToolsExtension? window.devToolsExtension() : f=>f;
+//const reduxDevTools = window.devToolsExtension? window.devToolsExtension() : f=>f;
 
 const store = createStore(
   counter,
-  compose(
-    applyMiddleware(thunk),
-    reduxDevTools
-    //window.devToolsExtension? window.devToolsExtension() : f=>f
-  )
+  composeEnhancers(applyMiddleware(thunk))
 );
-
+function Erying(){
+  return <h2>二营</h2>
+}
+function Qibinglian(){
+  return <h2>骑兵连</h2>
+}
+class Test extends React.Component{
+  render(){
+    console.log(this.props);
+    return(
+      <h1>测试组件{this.props.match.params.location}</h1>
+    )
+  }
+}
 ReactDom.render(
   (
     <Provider store={store}>
-      <App1 />,
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to='/'>一营</Link>
+            </li>
+            <li>
+              <Link to='/erying'>二营</Link>
+            </li>
+            <li>
+              <Link to='/qibinglian'>骑兵连</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path='/' exact component={App1}></Route>
+            <Route path='/erying' component={Erying}></Route>
+            <Route path='/qibinglian' component={Qibinglian}></Route>
+            <Route path='/:location'  component={Test}></Route>
+            {/* <App1 /> */}
+          </Switch>
+          </div>
+      </BrowserRouter>
     </Provider>
   ),
   document.getElementById('root')
