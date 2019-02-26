8c5f89d38d3dfda21a2df0f83a8d8e85e4899e86
zhongxiaodong <zxdzyadmin@yonyou.com>
2019-02-26 17:22:42 +0800

    修改及新增

---
+++ reactnode/src/container/register/register.js
@@ -0,0 +1,52 @@
+/*
+ * @Author: zhongxd 
+ * @Date: 2019-02-26 16:16:23 
+ * @Last Modified by: zhongxd
+ * @Last Modified time: 2019-02-26 16:57:37
+ */
+
+
+import React from 'react';
+import Logo from '../../component/logo/logo';
+import { List , InputItem , Button , WingBlank , WhiteSpace , Radio} from 'antd-mobile';
+
+class Register extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      type:'genius'
+    };
+
+    this.register = this.register.bind(this);
+  }
+
+  register(){
+    console.log(this.props);
+    this.props.history.push('/register');
+  }
+
+  render(){
+    const RadioItem = Radio.RadioItem;
+    return(
+      <div>
+        <Logo></Logo>
+        <WingBlank>
+          <List >
+            <InputItem>手机号码</InputItem>
+            <InputItem>密码</InputItem>
+            <InputItem>确认密码</InputItem>
+            <WhiteSpace />
+            <RadioItem checked={this.state.type=='genius'}>牛人</RadioItem>
+            <RadioItem checked={this.state.type=='boss'}>BOSS</RadioItem>
+          </List>
+          <Button type='primary'>登录</Button>
+          <WhiteSpace />
+          <Button type='primary' onClick={this.register}>注册</Button>
+        </WingBlank>
+      </div>
+    )
+  }
+
+}
+
+export default Register;
